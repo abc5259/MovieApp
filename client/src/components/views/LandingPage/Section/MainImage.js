4a8db6dad5dd1db922250673398b1105a2a61604
abc5259 <dlwogns3413@naver.com>
2020-05-27 00:32:43 +0900

    MovieApp#3

--- client/src/components/views/LandingPage/Section/MainImage.js
+++ client/src/components/views/LandingPage/Section/MainImage.js
@@ -3,13 +3,14 @@ import React from "react";
 function MainImage(props) {
   return (
     <div
+      className="background"
       style={{
         background: `linear-gradient(to bottom, rgba(0,0,0,0)
-    39%,rgba(0,0,0,0)
-    41%,rgba(0,0,0,0.65)
-    100%),
-    url('${props.image}'), #1c1c1c`,
-        height: "770px",
+        39%,rgba(0,0,0,0)
+        41%,rgba(0,0,0,0.65)
+        100%),
+        url('${props.image}'), #1c1c1c`,
+        height: "500px",
         backgroundSize: "100%, cover",
         backgroundPosition: "center, center",
         width: "100%",
@@ -27,6 +28,7 @@ function MainImage(props) {
         >
           <h2 style={{ color: "white" }}> {props.title} </h2>
           <p style={{ color: "white", fontSize: "1rem" }}>
+            {" "}
             {props.description}
           </p>
         </div>
@@ -34,4 +36,5 @@ function MainImage(props) {
     </div>
   );
 }
+
 export default MainImage;
